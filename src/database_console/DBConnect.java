42ee9676706598657bb348252d558cd1d2a1fc94
Johnny Casual <johnniecasual@gmail.com>
2015-01-18 20:23:00 +0200

    Created Database connection-class

---
+++ src/database_console/DBConnect.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database_console;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Joni Kämppä 18.12015
+ * This class is used for the database connection.
+ */
+public class DBConnect {
+    
+}
